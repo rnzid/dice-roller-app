4533aaafa84528b46c771e2a256259e25001187f
rnzid <ranjitdangol13@gmail.com>
2022-05-12 22:19:31 +0545

    working on dice image and button

--- src/App.js
+++ src/App.js
@@ -3,21 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+         <img width={300} height={300} src={require('./assets/1.png')}></img>
+         <br/>
+         <button className='button'>Roll</button>
+      </center>
     </div>
   );
 }
